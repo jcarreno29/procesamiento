538216605f935bc8c96801c06e164ca8b921de92
jcarreno29 <j.carreno29@gmail.com>
2026-01-18 14:56:02 -0300

    Add files via upload

---
+++ Instalacion.sh
@@ -0,0 +1,6 @@
+# 1. Crear entorno virtual (opcional pero recomendado)
+python -m venv venv
+source venv/bin/activate  # En Windows: venv\Scripts\activate
+
+# 2. Instalar dependencias
+pip install -r requirements.txt
